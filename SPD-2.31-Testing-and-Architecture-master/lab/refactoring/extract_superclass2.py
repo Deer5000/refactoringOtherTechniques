182723b62847db7f31684de0cc432e345469d683
Deer5000 <khidrbrinkley32@gmail.com>
2021-03-03 12:21:46 -0500

    finished all exercises

--- SPD-2.31-Testing-and-Architecture-master/lab/refactoring/extract_superclass2.py
+++ SPD-2.31-Testing-and-Architecture-master/lab/refactoring/extract_superclass2.py
@@ -1,36 +1,42 @@
 # by Kami Bigdely
 # Extract superclass.
-class Circle:
-    
+
+class Shape:
+    def __init__(self,x,y, visible=True):
+        self.x = x
+        self.y = y
+        self.visible = visible
+
+    def display(self, message):
+        print(message)
+
+class Circle(Shape):   
     def __init__(self, x, y, r, visible = True):
-      self.center_x = x
-      self.center_y = y
-      self.r = r
-      self.visible = visible
+        super(Circle, self).__init__(x, y, visible)
+        self.r = r
+        self.message = "Circle Created"
       
     def display(self):
-        print('drew the circle.')
+        super(Circle, self).display(self.message)
         
     def set_visible(self,is_visible):
         self.visible = is_visible
         
     def get_center(self):
-        return self.center_x, self.center_y
+        return self.x, self.y
     
     
-class Rectangle:
+class Rectangle(Shape):
     
     def __init__(self, x, y, width, height, visible = True):
-        # left-bottom corner.
-        self.x = x
-        self.y = y
+        super(Rectangle, self).__init__(x, y, visible)
+        self.message = "Rectangle Created"
         self.width = width
         self.height = height
-        self.visible = visible
         
     def display(self):
-        if self.visible:
-            print('drew the rectable.')
+        super(Rectangle, self).display(self.message)
+            
             
     def hide(self):
         self.visible = False
